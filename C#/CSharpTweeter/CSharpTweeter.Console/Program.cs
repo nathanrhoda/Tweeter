3d47a9dfca033f147f331ca5aa349460ea0ea8c5
Nathan Rhoda <nathan.rhoda@hotmail.com>
2016-10-12 21:26:35 +0200

    Created Console App and initial Test Project

---
+++ C#/CSharpTweeter/CSharpTweeter.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTweeter.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Tweet Tweet");
+            System.Console.ReadLine();
+
+        }
+    }
+}
---
+++ C#/CSharpTweeter/CSharpTweeter.Domain.Tests/FeedServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpTweeter.Domain.Tests
+{
+    [TestClass]
+    public class FeedServiceTest
+    {
+        [TestMethod]
+        public void GetFeeds_WhereFeedsExist_ReturnsAListOfFeeds()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
