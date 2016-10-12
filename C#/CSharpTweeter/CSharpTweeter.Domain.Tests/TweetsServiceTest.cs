f2bf7a9cc9ee507c72345c61aea76d5fa43b5a5a
Nathan Rhoda <nathan.rhoda@hotmail.com>
2016-10-12 21:58:20 +0200

    Files for tweets and users added Creation of high level test list

---
+++ C#/CSharpTweeter/CSharpTweeter.Domain.Tests/TweetsServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpTweeter.Domain.Tests
+{
+    [TestClass]
+    public class TweetsServiceTest
+    {
+        [TestMethod]
+        public void GetTweets_WhereFeedsExist_ReturnsAListOfFeeds()
+        {
+            var service = new TweetsService();
+            var tweets = service.GetTweets();
+            Assert.IsNotNull(tweets);
+        }
+    }
+
+    public class TweetsService
+    {
+        public object GetTweets()
+        {
+            return new object();
+        }
+    }
+}
