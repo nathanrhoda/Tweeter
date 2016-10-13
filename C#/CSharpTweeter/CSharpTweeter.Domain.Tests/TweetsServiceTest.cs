0a18f4853bc3a4e2e8e29956f3e7dc8beb1646ef
Nathan Rhoda <nathan.rhoda@hotmail.com>
2016-10-13 22:27:11 +0200

    Removed Unecessary Tests Updated Testlist Added functionality to read from file Added tests to initialize service

--- C#/CSharpTweeter/CSharpTweeter.Domain.Tests/TweetsServiceTest.cs
+++ C#/CSharpTweeter/CSharpTweeter.Domain.Tests/TweetsServiceTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using CSharpTweeter.Domain;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CSharpTweeter.Domain.Tests
@@ -7,20 +9,46 @@ namespace CSharpTweeter.Domain.Tests
     [TestClass]
     public class TweetsServiceTest
     {
-        [TestMethod]
-        public void GetTweets_WhereFeedsExist_ReturnsAListOfFeeds()
+        [TestMethod, Ignore]
+        public void GetTweets_WhereFeedsExist_ReturnsMoreThanZeroTweets()
         {
-            var service = new TweetsService();
+            var service = TweetsServiceFactory.Tweets(); 
             var tweets = service.GetTweets();
-            Assert.IsNotNull(tweets);
+            Assert.IsTrue(tweets.Count > 0);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Initialize_WhereInputIsNotValid_ThrowsException()
+        {
+            var service = TweetsServiceFactory.InvalidInputFiles();            
+                        
+        }
+
+        [TestMethod]
+        public void Initialize_WhereInputIsValid_ReturnsUsersAndTweets()
+        {
+            var service = TweetsServiceFactory.Tweets();
+
+            Assert.IsTrue(service.Tweets.Count > 0);
+            Assert.IsTrue(service.Users.Count > 0);
+        }
+    }    
+}
+
+
+public static class TweetsServiceFactory
+{
+    public static TweetsService InvalidInputFiles()
+    {
+        return new TweetsService("", "");
     }
 
-    public class TweetsService
+    public static TweetsService Tweets()
     {
-        public List<object> GetTweets()
-        {
-            return new List<object>();                                                
-        }
+        var tweetsFilePath = ConfigurationManager.AppSettings["TweetFilePath"];
+        var userFilePath = ConfigurationManager.AppSettings["UserFilePath"];
+        return new TweetsService(tweetsFilePath, userFilePath);
     }
+
 }
